7b5d0c4612685bf08a27969342243563097e46f4
Wloy12345 <wloygames@gmail.com>
2024-07-27 15:06:32 -0400

    reset

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import { MotionPlugin } from '@vueuse/motion';
-import './style.css';
-import './styles.css'
-import App from './App.vue';
-import router from './router';
-
-createApp(App).use(MotionPlugin).use(router).mount('#app');
-
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/GBC/',
-  plugins: [vue()],
-  publicPath: process.env.NODE_ENV === "production" ? "/GBC/" : "/",
-})
